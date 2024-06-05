1497823dc017969a69e30debcc73fb32c4061192
Gromototo <rodlpd51@gmail.com>
2024-06-05 14:49:18 +0200

    automaton base not tested

--- Game.Prototype/src/game/automaton/State.java
+++ Game.Prototype/src/game/automaton/State.java
@@ -1,5 +1,30 @@
 package game.automaton;
 
-public interface State {
+import java.util.List;
 
+public class State {
+
+	private int tirage = 0;
+	private List<Transition> transitionList;
+	
+	State(List<Transition> tList) {
+		transitionList = tList;
+	}
+	
+	List<Transition>get_transitionList() {
+		return transitionList;
+	}
+	
+	void set_transitionList(List<Transition> transitionList) {
+		this.transitionList =  transitionList;
+	}
+	
+	boolean add_transition(Transition t) {
+		return transitionList.add(t);
+	}
+	
+	boolean remove_transition(Transition t) {
+		return transitionList.remove(t);
+	}
+	
 }
--- Game.Prototype/src/game/automaton/Transition.java
+++ Game.Prototype/src/game/automaton/Transition.java
@@ -1,9 +1,18 @@
 package game.automaton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Transition {
-	State source, cible;
+	State cible;
 	Condition c;
-	Action a;
+	List<Action> actionList;
 	
+	Transition(State cible, Condition condition, List<Action> a) {
+		
+		actionList = a;
+		this.cible = cible;
+		this.c = condition;
+	}
 	
 }
