468d6e19887abafef7293d9ce397b8ce6cea4a26
hafraoui <ines.hafraoui@etu.univ-grenoble-alpes.fr>
2024-06-06 09:51:04 +0200

    a few changes to the grid

--- info3.game.given.2021/game/info3/game/snake/Grid.java
+++ info3.game.given.2021/game/info3/game/snake/Grid.java
@@ -24,23 +24,27 @@ public class Grid {
 	}
 
 	public void paint(Graphics g) {
+		Color[] colorpos = GetEltsPos();
 		int box_width = (m_width + m_border) / m_nboxline;
 		int box_height = (m_height - m_border) / m_nboxcol;
 		for (int i = 0; i < m_nboxcol; i++) {
 			for (int j = 0; j < m_nboxline; j++) {
-				if (GridEltAtPos(i, j))
-					g.setColor(Color.BLUE);
-				else
+				if(colorpos[i*j] == null)
 					g.setColor(Color.DARK_GRAY);
 				g.fillRect(m_x + j * box_width + m_border, m_y + i * box_height + m_border, box_width - (m_border * 2),
 						box_height - m_border);
 			}
 		}
 	}
-
-	boolean GridEltAtPos(int col, int line) {
+	
+	/* This method returns an array, size of the grid with the color of the element
+	 * present on this box if there is nothing its null
+	 */
+	Color[] GetEltsPos() {
+		Color[] colortab = new Color[m_nboxcol*m_nboxline];
 		//un itérateur sur les élements, on cherche à ce que chacun des elements nous renvoie sa position
-		return false;
+		//et on remplie le tableau en conséquence
+		return colortab;
 	}
 
 }
