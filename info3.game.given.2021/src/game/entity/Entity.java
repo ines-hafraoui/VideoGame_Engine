06aa68cf2e0f4a644046ac25bf46febb0ddf6506
Gromototo <rodlpd51@gmail.com>
2024-06-06 11:57:40 +0200

    Linked tick to entites

--- info3.game.given.2021/src/game/entity/Entity.java
+++ info3.game.given.2021/src/game/entity/Entity.java
@@ -28,4 +28,11 @@ public abstract class Entity {
 	public abstract boolean do_move();
 
 	public abstract boolean do_egg();
+	
+	public void tick(long elpased) {
+		
+		if (aut != null) {
+			aut.step(this);
+		}
+	}
 }
