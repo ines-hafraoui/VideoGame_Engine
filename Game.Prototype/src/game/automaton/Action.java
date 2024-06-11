99c0c19ebb675fe68fa562d23c1e5ae57df5ae65
hafraoui <ines.hafraoui@etu.univ-grenoble-alpes.fr>
2024-06-11 14:25:41 +0200

    deleting the game prototype package

--- Game.Prototype/src/game/automaton/Action.java
+++
@@ -1,7 +0,0 @@
-package game.automaton;
-
-import game.entity.Entity;
-
-public interface Action {
-	boolean exec(Entity e);
-}
--- Game.Prototype/src/game/automaton/Condition.java
+++
@@ -1,7 +0,0 @@
-package game.automaton;
-
-import game.entity.Entity;
-
-public interface Condition {
-	boolean eval (Entity e);
-}
