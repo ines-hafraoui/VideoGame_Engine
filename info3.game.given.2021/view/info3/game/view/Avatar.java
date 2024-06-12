1ba04878e4f83af7d9e2ff5a61fb20333265cb5a
Nathanaël Chauvin <chauvnat@etu.univ-grenoble-alpes.fr>
2024-06-12 16:52:39 +0200

    Test sprite on player avatar

--- info3.game.given.2021/view/info3/game/view/Avatar.java
+++ info3.game.given.2021/view/info3/game/view/Avatar.java
@@ -3,6 +3,11 @@ package info3.game.view;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import game.entity.Apple;
 import game.entity.Block;
@@ -13,7 +18,9 @@ public abstract class Avatar extends Component{
 
 	Entity m_entity;
 	View m_view;
-
+	BufferedImage[] m_images;
+	int m_imageIndex;
+	
 	public Avatar(Entity e, View v) {
 		m_view = v;
 		m_entity = e;
@@ -21,4 +28,42 @@ public abstract class Avatar extends Component{
 	}
 
 	public abstract void paint(Graphics g, int box_width, int box_height);
+	
+	public static BufferedImage[] loadSprite(String filename, int nrows, int ncols) throws IOException {
+	    File imageFile = new File(filename);
+	    if (imageFile.exists()) {
+	      BufferedImage image = ImageIO.read(imageFile);
+	      int width = image.getWidth(null) / ncols;
+	      int height = image.getHeight(null) / nrows;
+
+	      BufferedImage[] images = new BufferedImage[nrows * ncols];
+	      for (int i = 0; i < nrows; i++) {
+	        for (int j = 0; j < ncols; j++) {
+	          int x = j * width;
+	          int y = i * height;
+	          images[(i * ncols) + j] = image.getSubimage(x, y, width, height);
+	        }
+	      }
+	      return images;
+	    }
+	    return null;
+	  }
+	
+	protected void drawHealthBar(Graphics g, int x, int y, int width, int height) {
+        //int health= m_entity.health;
+		int health = 50;
+        int healthWidth = (int) ((width * health) / 100.0);
+        
+        // Fond de la barre de santé
+        g.setColor(Color.GRAY);
+        g.fillRect(x, y, width, height);
+
+        // Barre de santé actuelle
+        g.setColor(Color.BLUE);
+        g.fillRect(x, y, healthWidth, height);
+
+        // Bordure de la barre de santé
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, width, height);
+    }
 }
---
+++ info3.game.given.2021/view/info3/game/view/PlayerAvatar.java
@@ -0,0 +1,34 @@
+package info3.game.view;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import game.entity.Apple;
+
+public class PlayerAvatar extends Avatar{
+
+	public static final int HIT = 1;
+	public static final int WALK = 2;
+	
+	public PlayerAvatar(Player e, View v) {
+		super(e,v);
+		m_imageIndex=0;
+		loadSprite("resources/perso.png", 8, 12);
+	}
+
+	@Override
+	public void paint(Graphics g, int box_width, int box_height) {
+		int etat=2;
+		BufferedImage img = m_images[m_imageIndex];
+		g.drawImage(img, m_view.get_x() + m_entity.get_x(), m_view.get_y() + m_entity.get_y(), img.getWidth(), img.getHeight(), m_view);
+		drawHealthBar(g,m_view.get_x() + m_entity.get_x(),m_view.get_y() + m_entity.get_y() - 5%img.getHeight(),img.getWidth(),5%img.getHeight());
+	}
+	
+	
+	
+
+}
