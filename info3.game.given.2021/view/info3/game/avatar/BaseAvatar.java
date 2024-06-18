b6a4f13cb258f39290e36822c8cfd34fbea32cd0
Nathanaël Chauvin <chauvnat@etu.univ-grenoble-alpes.fr>
2024-06-18 14:28:58 +0200

    Fin avatar

--- info3.game.given.2021/view/info3/game/avatar/BaseAvatar.java
+++ info3.game.given.2021/view/info3/game/avatar/BaseAvatar.java
@@ -8,10 +8,10 @@ import java.io.IOException;
 import game.entity.Entity;
 import info3.game.view.View;
 
-public class BaseAvatar extends Avatar{
+public class BaseAvatar extends Avatar {
 
 	public BaseAvatar(Entity e, View v) throws IOException {
-		super(e,v);
+		super(e, v);
 		m_imageIndex = 0;
 		m_images = View.loadSprite("resources/MiniWorldSprites/Buildings/Red/RedKeep.png", 2, 3);
 	}
@@ -19,6 +19,7 @@ public class BaseAvatar extends Avatar{
 	@Override
 	public void paint(Graphics g, int x, int y) {
 		BufferedImage img = m_images[m_imageIndex];
-		g.drawImage(img, x + (int)m_entity.get_x()-(img.getWidth() * View.DISPLAYSCALE), y + (int)m_entity.get_y()-(img.getHeight() * View.DISPLAYSCALE), img.getWidth() * View.DISPLAYSCALE, img.getHeight() * View.DISPLAYSCALE, null);
-}
+		g.drawImage(img, x + (int) m_entity.get_x(), y + (int) m_entity.get_y(), img.getWidth() * View.DISPLAYSCALE,
+				img.getHeight() * View.DISPLAYSCALE, null);
+	}
 }
